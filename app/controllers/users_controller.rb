0ec9c325c2860bd8f6e05513e4849c808d14dd43
SakamotoMasaya <masayasakamoto9@gmail.com>
2019-11-04 22:18:07 +0900

    signup createアクション 追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,21 @@ class UsersController < ApplicationController
   end
 
   def signup
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redairect_to root_path, success: '登録が完了しました'
+    else
+      flash.now[:danger] = "登録に失敗しました"
+      render :signup
+    end
+  end
+
+  privete
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
