b9ff7a25b7df32f8eacbefb0fc229d72093783b8
SakamotoMasaya <masayasakamoto9@gmail.com>
2019-11-04 21:23:52 +0900

    新規登録　情報入力　追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get 'pages/top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  get 'users/new/email', to; 'users#email'
+
+  resources :users do
+    collection do
+      get :signup
+    end
+  end
 
   root 'pages#top'
 end
