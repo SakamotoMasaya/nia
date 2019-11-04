aa91f3b9cd6e631b55c2a0f7118bc1a4e18ee510
SakamotoMasaya <masayasakamoto9@gmail.com>
2019-11-04 14:09:03 +0900

    assets.reの編集
    ページごとにcssを割り当てるため

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
   get 'pages/top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 
   root 'pages#top'
 end
