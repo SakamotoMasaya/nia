5e6b3bedd690d11541b9c01c43b82daeddab9c4f
SakamotoMasaya <masayasakamoto9@gmail.com>
2019-11-05 23:22:49 +0900

    新規登録 情報入力欄 バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: { with: /\A[\w.]+@[a-z\d.-]+\.[a-zA-Z0-9-]+\z/ }
+  validates :password, length: { in: 7..50 }, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
+  # 文字列の開始位置　a-zA-Zを含むかつ\dを含む a-zA-z\dの中から　文字列の終了位置
 
-    has_secure_password
+  has_secure_password
 end
