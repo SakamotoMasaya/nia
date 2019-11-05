68420fd4c4ae33837b45844baeb28642d36c128d
SakamotoMasaya <masayasakamoto9@gmail.com>
2019-11-05 21:38:00 +0900

    form_for　修正
    formタグの中にform_forを書き　誤動作した為

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,24 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def signup
+  end
+  
+  def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redairect_to root_path, success: '登録が完了しました'
+      redirect_to root_path, success: '登録が完了しました'
     else
       flash.now[:danger] = "登録に失敗しました"
-      render :signup
+      render :new
     end
   end
 
   private
+  
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password)
   end
 end
