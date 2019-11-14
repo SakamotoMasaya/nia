f5965ef2a98cf13994665ee82d7da0950bd1be9a
SakamotoMasaya <masayasakamoto9@gmail.com>
2019-11-14 20:49:21 +0900

    follow機能　実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,30 +2,30 @@ class RelationshipsController < ApplicationController
   # before_action :set_user
 
   def create
-    user = User.find(params[:relationship][:follow_id])
+    user = User.find(params[:follow_id])
     following = current_user.follow(user)
     if following.save
-      flash[:success] = 'フォローしました'
+      flash[:success] = 'ユーザーをフォローしました'
       redirect_to user
     else
-      flash.now[:danger] = 'フォローに失敗しました'
+      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
       redirect_to user
     end
   end
 
   def destroy
-    user = User.find(params[:relationship][:follow_id])
+    user = User.find(params[:follow_id])
     following = current_user.unfollow(user)
     if following.destroy
-      flash[:success] = 'フォローを解除しました'
+      flash[:success] = 'ユーザーのフォローを解除しました'
       redirect_to user
     else
-      flash.now[:danger] = 'フォローの解除に失敗しました'
+      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
       redirect_to user
     end
   end
 
-  # private
+  private
 
   # def set_user
   #   user = User.find(params[:relationship][:follow_id])
