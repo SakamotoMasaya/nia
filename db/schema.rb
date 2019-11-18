1f34cb72ccdc18f85881dc979e3e355a372954a7
SakamotoMasaya <masayasakamoto9@gmail.com>
2019-11-18 19:39:37 +0900

    item_imgagesモデル generate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_133224) do
+ActiveRecord::Schema.define(version: 2019_11_18_103423) do
+
+  create_table "item_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "item_id"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
