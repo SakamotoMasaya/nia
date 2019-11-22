e4a95415b5c824ee05342d5936a2a0d35af7fb1b
SakamotoMasaya <masayasakamoto9@gmail.com>
2019-11-23 08:08:21 +0900

    Categoriesモデルgenerate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_151006) do
+ActiveRecord::Schema.define(version: 2019_11_22_230601) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
