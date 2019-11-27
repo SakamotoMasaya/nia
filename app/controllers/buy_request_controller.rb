8e91b5756797eb7bd6300f0ea2bb48e9d586995e
SakamotoMasaya <masayasakamoto9@gmail.com>
2019-11-27 20:55:28 +0900

    商品購入　実装

--- app/controllers/buy_request_controller.rb
+++ app/controllers/buy_request_controller.rb
@@ -1,7 +1,14 @@
 class BuyRequestController < ApplicationController
-  def index
-  end
+  def index; end
 
   def create
+    buyer = Item.find_by(id: params[:item_id])
+    buyer.buyer_id = current_user.id
+    if buyer.save!
+      redirect_to root_url, success: '購入しました'
+    else
+      flash.now[:danger] = '購入に失敗しました'
+      redirect_to item_path(id: params[:item_id]), danger: '購入に失敗しました'
+    end
   end
 end
